69048738551555cf2f8054783a4390b9bdde8222
EarthlySolo <158605990+EarthlySolo@users.noreply.github.com>
2024-02-10 00:59:36 +0100

    refactor

--- bioenergeticbot.py
+++ bioenergeticbot.py
@@ -10,20 +10,63 @@ import logging
 from profanity_check import predict
 
 
-def make_tag(tag: str):
-    """make tags usable for twitter"""
-    # split at occurence of nonalphanumeric
-    words = re.split(r'[\W_]+', tag)
-    words = [word.capitalize() for word in words]
-    return '#' + ''.join(words)
-    pass
-
-
 logging.basicConfig(filename='reposter.log',
                     level=logging.INFO,
                     format='%(asctime)s %(levelname)s: %(message)s',
                     datefmt='%d.%m.%y %H:%M:%S %Z'
                     )
+
+base_url = "https://bioenergetic.forum/"
+api_url = base_url + "api/recent/posts/day"
+
+
+def make_tag(tag: str):
+    """make tags usable for twitter"""
+    # split at occurrence of non-alphanumeric
+    words = re.split(r'[\W_]+', tag)
+    words = [word.capitalize() for word in words]
+    return '#' + ''.join(words)
+
+
+def search_topics() -> list:
+    """need forum account for search"""
+    search_url = base_url + "api/search?in=titles&term=&matchWords=all&by=&categories=&searchChildren=false&hasTags" \
+                            "=&replies=&repliesFilter=atleast&timeFilter=newer&timeRange=86400&sortBy=topic.timestamp" \
+                            "&sortDirection=desc&showAs=topics&page=1"
+    login_data = {"username": "",
+                  "password": ""}
+    with requests.Session() as s:
+        s.post(base_url + "api/v3/utilities/login", json=login_data)
+        r = requests.post(search_url).json()
+        posts = r["posts"]
+    pass
+
+
+def recent_posts() -> list:
+    # find all new topics since the last run using save_data['tid']
+    new_topic_list = []
+    found = False
+    for page in range(1, 6):
+        time.sleep(5)
+        request_url = api_url + "?page={}".format(page)
+        try:
+            posts = requests.request("GET", request_url).json()
+            logging.info(f"Sent request to {request_url}")
+        except json.JSONDecodeError:
+            logging.error("Error decoding JSON.", exc_info=True)
+            raise
+        # save all the main posts starting from page 1 of recent posts
+        for post in posts:
+            if post['isMainPost']:
+                if post['tid'] <= save_data['tid']:
+                    found = True
+                    break
+                # if tid not found, it's a new topic, save it
+                if post not in new_topic_list:
+                    new_topic_list.append(post)
+        if found:
+            break
+    return new_topic_list
 
 
 # json for x-rate-limit-reset time (epoch) and most recently posted topic id
@@ -43,39 +86,12 @@ client = tweepy.Client(
     # to access x-rate-limit-remaining header. probably breaks access to response.data['id']
     # return_type=requests.Response
 )
-base_url = "https://bioenergetic.forum/"
-api_url = base_url + "api/recent/posts/day"
 
-# there's no way to only list new topics chronologically
-# find all new topics since the last run using save_data['tid']
-new_topic_list = []
-found = False
-# arbitrary limit
-for page in range(1, 6):
-    time.sleep(5)
-    request_url = api_url + "?page={}".format(page)
-    try:
-        recent_posts = requests.request("GET", request_url).json()
-        logging.info(f"Sent request to {request_url}")
-    except json.JSONDecodeError:
-        logging.error("Error decoding JSON.", exc_info=True)
-        continue
-    # save all the main post starting from page 1 of recent posts
-    for post in recent_posts:
-        if post['isMainPost']:
-            if post['tid'] <= save_data['tid']:
-                found = True
-                break
-            # if tid not found, it's a new topic, save it
-            if post not in new_topic_list:
-                new_topic_list.append(post)
-    if found:
-        break
-
-if not new_topic_list:
+new_topics = recent_posts()
+if not new_topics:
     logging.info("No new topics.")
 
-for topic in reversed(new_topic_list):
+for topic in reversed(new_topics):
     title = html.unescape(topic['topic']['titleRaw'])
     author = html.unescape(topic['user']['displayname'])
     link = base_url + "topic/" + str(topic['tid'])
@@ -93,7 +109,6 @@ for topic in reversed(new_topic_list):
         logging.info(f"Filtered bad words in:\n {[term[i] for i in pred[:] if pred[i]]}.\n{topic['tid']} not posted.")
         continue
     if category in ignore_categories:
-        # ignore category
         logging.info(f"Ignored category {category}. {topic['tid']} not posted.")
         continue
     try:
@@ -123,7 +138,7 @@ for topic in reversed(new_topic_list):
         logging.error(f"Error sending Tweet (tid {topic['tid']}).", exc_info=True)
 
 with open('save_data.json', 'w') as writer:
-    if len(new_topic_list) > 0:
-        save_data['tid'] = new_topic_list[0]['tid']
+    if len(new_topics) > 0:
+        save_data['tid'] = new_topics[0]['tid']
         logging.info(f"Saved most recent topic id: {save_data['tid']}")
     json.dump(save_data, writer)
